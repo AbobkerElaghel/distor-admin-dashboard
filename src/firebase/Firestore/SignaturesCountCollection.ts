906aa5d35c300f7b210eec9d721df584a3765c1a
abobkerelaghel <abobkerelaghel@gmail.com>
2022-07-27 11:16:10 +0200

    modified (be aware of the addFile file changes

--- src/firebase/Firestore/SignaturesCountCollection.ts
+++ src/firebase/Firestore/SignaturesCountCollection.ts
@@ -1,6 +1,13 @@
-import { doc, increment, updateDoc } from "firebase/firestore";
+import { doc, getDoc, increment, updateDoc } from "firebase/firestore";
 import firestore from '../firestore';
 
 export const decrementSignaturesCount = () => {
-    return updateDoc(doc(firestore, "signaturesCount", "count"), { count: increment(1) });
+    return getDoc(doc(firestore, "signaturesCount", "count"))
+        .then((document) => {
+            if (document.exists()) {
+                if (document.data().count && document.data().count > 0) {
+                    return updateDoc(doc(firestore, "signaturesCount", "count"), { count: increment(-1) });
+                }
+            }
+        })
 };
